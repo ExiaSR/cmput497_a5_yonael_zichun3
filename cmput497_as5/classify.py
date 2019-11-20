6622762172c2e3e1f541c388d1e5f4c769888dc0
Michael Lin <hi@michaellin.me>
2019-11-20 14:45:28 -0700

    Add naive bayes text classifier train and test (#1)
    * add naive bayes text classifier train and test
    
    * remove nltk API with built-in methods

--- cmput497_as5/classify.py
+++ cmput497_as5/classify.py
@@ -1,26 +1,51 @@
-from collections import defaultdict
+from operator import itemgetter
+from collections import defaultdict, OrderedDict, Counter
+from functools import reduce
 
-from nltk.probability import FreqDist
+import numpy as np
 
 
 class NaiveBayesClassifier:
-    def __init__(self, label_probdist, word_probdist):
-        self._label_probdist = label_probdist
-        self._word_probdist = word_probdist
+    def __init__(self, log_prior, log_likelihood, classes, vocabs):
+        self._log_prior = log_prior
+        self._log_likelihood = log_likelihood
+        self._vocabs = vocabs
+        self._classes = classes
 
     @classmethod
-    def train(cls, dataset):
+    def train(cls, dataset: "Dataset"):
         """
         c -> class/label
         w -> word
+
+        :param dataset: A Dataset object
         """
-        label_freqdist = FreqDist()  # P(c) or log_prior
-        word_freqdist = defaultdict(FreqDist)  # P(w|c) or log_likelihood
+        # count number of occurences of each word given their originated label
+        labled_featuresets = dataset.get_labeled_featuresets()
+        big_doc = defaultdict(Counter)
+        for word, label in labled_featuresets:
+            big_doc[label][word] += 1
+        # get vocabs
+        vocabs: set = dataset.vocabs()
+        # get labels
+        classes = list(dataset.classes())
+        # count number of documents in dataset in each class
+        label_freqdist = Counter(dataset.labels())
+        # total number of documents in dataset
+        num_docs = sum(label_freqdist.values())
 
-        # TODO
-        # implement algorithm descripes in Fig 4.2 on p4 ch4
+        log_prior = dict()
+        log_likelihood = defaultdict(dict)  # avoid key error
+        for class_name in classes:
+            num_docs_in_class = label_freqdist[class_name]
+            log_prior[class_name] = np.log2(label_freqdist[class_name] / num_docs)
+            denominator = sum(big_doc[class_name].values()) + len(vocabs)
+            for word in vocabs:
+                count = big_doc[class_name][word]  # number of occurrences of w in bigdoc[c]
+                numerator = count + 1
+                log_likelihood[class_name][word] = np.log2(numerator / denominator)
 
-        return cls(label_freqdist, word_freqdist)
+        return cls(log_prior, log_likelihood, classes, vocabs)
 
     def classify(self, datasets: list) -> list:
         """
@@ -28,4 +53,15 @@ class NaiveBayesClassifier:
         :return: Return a list of predicted labels.
         :rtype: ``list``
         """
-        pass
+        predicted_labels = []
+        for document in datasets:
+            log_sum = {}
+            for class_name in self._classes:
+                log_sum[class_name] = self._log_prior[class_name]
+                for word in document:
+                    if word in self._vocabs:
+                        log_sum[class_name] += self._log_likelihood[class_name][word]
+
+            predicted_labels.append(max(log_sum, key=log_sum.get))
+
+        return predicted_labels
--- cmput497_as5/utils.py
+++ cmput497_as5/utils.py
@@ -1,5 +1,10 @@
 import csv
 import os
+import errno
+
+from collections import Counter
+
+flatten = lambda l: [item for sublist in l for item in sublist]
 
 
 class Dataset:
@@ -8,24 +13,49 @@ class Dataset:
         self._path = file_path
         self._filename = os.path.splitext(os.path.basename(file_path))[0]
         self._labels = zipped[0]  # original labels
-        self._predicted_labels = [] # assigned predicted labels
+        self._predicted_labels = []  # assigned predicted labels
+        self._text = zipped[1]
         if preprocesser:
-            self._text = list(map(preprocesser, zipped[1]))
+            self._documents = list(map(preprocesser, zipped[1]))
         else:
             # dummpy tokenizer for testing purpose
-            self._text = list(map(lambda x: x.split(), zipped[1]))
+            self._documents = list(map(lambda x: x.split(), zipped[1]))
+        self._document_by_class_counter = Counter([c for c in self._labels])
 
     def text(self):
         return self._text
 
-    def lables(self):
+    def labels(self):
         return self._labels
+
+    def classes(self):
+        return set(self._labels)
+
+    def tokens(self):
+        return flatten(self._documents)
+
+    def vocabs(self):
+        return set(flatten(self._documents))
+
+    def documents(self):
+        return self._documents
 
     def predicted_labels(self):
         return self._predicted_labels
 
+    def get_labeled_featuresets(self):
+        """
+        :return: A list of labeled word.
+        :rtype: ``[(word, label), (word, label)]``
+        """
+        labeled_featuresets = []
+        for tokens, label in zip(self._documents, self._labels):
+            labeled_featuresets.extend([(token, label) for token in tokens])
+        return labeled_featuresets
+
     def _set_predicted_labels(self, labels):
         self._predicted_labels = labels
+
 
 def tokenizer(document: str):
     """
@@ -37,6 +67,7 @@ def tokenizer(document: str):
     """
     # TODO
     pass
+
 
 def get_dataset(file_path) -> Dataset:
     """
@@ -52,6 +83,34 @@ def get_dataset(file_path) -> Dataset:
         return dataset
 
 
+def accuracy(predicted_labels, gold_labels):
+    """
+    Get accuracy of the classifier
+    """
+    correct = 0
+    for pred, gold in zip(predicted_labels, gold_labels):
+        if pred == gold:
+            correct += 1
+
+    return correct / len(predicted_labels)
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+# https://stackoverflow.com/a/23794010
+def safe_open_w(path, mode="wt"):
+    mkdir_p(os.path.dirname(path))
+    return open(path, mode)
+
+
 def save_output(dataset: Dataset, dir="output/"):
     """
     Save predicted output to disk
@@ -61,9 +120,9 @@ def save_output(dataset: Dataset, dir="output/"):
     if len(dataset._predicted_labels) <= 0:
         raise Exception("No predicted assigned label.")
 
-    with open(os.path.join(dir, "output_{}.csv"), "w") as output_f:
+    with safe_open_w(os.path.join(dir, "output_{}.csv".format(dataset._filename)), "w") as output_f:
         output_writer = csv.writer(output_f)
 
-        output_rows = list(zip(dataset.lables(), dataset.predicted_labels(), dataset.text()))
+        output_rows = list(zip(dataset.labels(), dataset.predicted_labels(), dataset.text()))
         output_rows.insert(0, ["category", "predictedCategory", "text"])
         output_writer.writerows(output_rows)
